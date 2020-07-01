5ba32f5c4b0a7511ba37022f085c0485468ecd86
Cadain Miller <cadainmiller@gmail.com>
2020-06-30 21:26:06 -0500

    Calcuation for lab1 display complated

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,11 @@ router.get('/lab1', function (req, res, next) {
   res.render('pages/lab1', { title: 'Express' });
 });
 
+router.get('/lab1_result', function (req, res, next) {
+  res.render('pages/lab1_result', { title: 'Express' });
+});
+
+
 router.get('/lab2', function (req, res, next) {
   res.render('pages/lab2', { title: 'Express' });
 });
@@ -34,8 +39,9 @@ router.post('/cal_lab1', urlencodedParser, function (req, res, next) {
   var itotal_1 = 0;
   var rtotal_2 = 0;
   var itotal_2 = 0;
-  var r13 = 0;
   var Ia, IaM = 0;
+  var Ib, IbM = 0;
+  var I3, I3M = 0;
 
   rtotal_1 = ((r3 * r2) / (r3 + r2)) + r1;
 
@@ -43,16 +49,45 @@ router.post('/cal_lab1', urlencodedParser, function (req, res, next) {
 
   Ia = itotal_1 * (r2 / (r2 + r3));
   IaM = (Ia * 1000).toFixed(4);
-
+  console.log('-----------------------------');
   console.log('R1: ' + r1);
   console.log('R2: ' + r2);
   console.log('R3: ' + r3);
+  console.log('-----------------------------');
   console.log('R Total: ' + rtotal_1);
   console.log('I Total: ' + itotal_1);
   console.log('IA: ' + Ia);
   console.log('Ia(mA): ' + IaM);
+  console.log('-----------------------------');
 
 
+  rtotal_2 = ((r1 * r3) / (r1 + r3)) + r2;
+
+  itotal_2 = e2 / rtotal_2;
+
+  Ib = itotal_2 * (r1 / (r1 + r3));
+  IbM = (Ib * 1000).toFixed(4);
+
+  I3 = Ia + Ib;
+  I3M = (I3 * 1000).toFixed(4);
+
+  console.log('-----------------------------');
+  console.log('R Total: ' + rtotal_2);
+  console.log('I Total: ' + itotal_2);
+  console.log('Ib: ' + Ib);
+  console.log('Ib(mA): ' + IbM);
+  console.log('-----------------------------');
+  console.log('I3: ' + I3);
+  console.log('I3(mA): ' + I3M);
+
+
+  res.render("pages/lab1_result", {
+    IaM: IaM,
+    IbM: IbM,
+    I3M: I3M,
+    e1: e1,
+    e2: e2
+  });
 
 });
 
