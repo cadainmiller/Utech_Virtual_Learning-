7281075380a21f403b6cbdda6c568e9a412058d3
Cadain Miller <cadainmiller@gmail.com>
2020-07-01 19:41:16 -0500

    Calcuation for Thevenin's Theorem [A] added

--- routes/index.js
+++ routes/index.js
@@ -13,17 +13,20 @@ router.get('/lab1', function (req, res, next) {
   res.render('pages/lab1', { title: 'Express' });
 });
 
-router.get('/lab1_result', function (req, res, next) {
-  res.render('pages/lab1_result', { title: 'Express' });
-});
-
-
 router.get('/lab2', function (req, res, next) {
   res.render('pages/lab2', { title: 'Express' });
 });
 
 router.get('/lab3', function (req, res, next) {
   res.render('pages/lab3', { title: 'Express' });
+});
+
+router.get('/lab1_result', function (req, res, next) {
+  res.render('pages/lab1_result', { title: 'Express' });
+});
+
+router.get('/lab2_result', function (req, res, next) {
+  res.render('pages/lab2_result', { title: 'Express' });
 });
 
 
@@ -87,6 +90,40 @@ router.post('/cal_lab1', urlencodedParser, function (req, res, next) {
     I3M: I3M,
     e1: e1,
     e2: e2
+  });
+
+});
+
+router.post('/cal_lab2', urlencodedParser, function (req, res, next) {
+  var r1 = parseFloat(req.body.r1);
+  var r2 = parseFloat(req.body.r2);
+  var r3 = parseFloat(req.body.r3);
+
+  var rl = parseFloat(req.body.rl);
+
+  var e1 = parseFloat(req.body.e1);
+  var e2 = parseFloat(req.body.e2);
+
+  var RT = 0;
+  var Rl_total = 0;
+  var I = 0;
+  var V = 0;
+  var Itotal = 0;
+  var IaM = 0;
+  var Vl1 = 0;
+
+  RT = (r1 * r2) / (r1 + r2);
+  Rl_total = (r3 * rl) / (r3 + rl);
+  I = (e2 - e1) / (r1 + r2);
+  V = e2 - (r2 * I);
+  Itotal = V / (RT + Rl_total);
+  IaM = (Itotal * 1000).toFixed(4);
+  Vl1 = (Itotal * Rl_total).toFixed(4);
+
+
+  res.render("pages/lab2_result", {
+    IaM: IaM,
+    Vl1: Vl1,
   });
 
 });
