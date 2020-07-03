2d1c128743f46ee49b8710feaedb717088e1793f
Cadain Miller - Work PC <cadainmiller@gmail.com>
2020-07-03 14:07:07 -0500

    Removing some console log used for test data

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-/* GET home page. */
+/* GET Routes Page. */
 router.get('/', function (req, res, next) {
   res.render('pages/index', { title: 'Express' });
 });
@@ -42,6 +42,7 @@ router.get('/lab4_result', function (req, res, next) {
 });
 
 
+/* Post Routes Page. */
 router.post('/cal_lab1', urlencodedParser, function (req, res, next) {
   var r1 = parseFloat(req.body.r1);
   var r2 = parseFloat(req.body.r2);
@@ -59,42 +60,15 @@ router.post('/cal_lab1', urlencodedParser, function (req, res, next) {
   var I3, I3M = 0;
 
   rtotal_1 = ((r3 * r2) / (r3 + r2)) + r1;
-
   itotal_1 = e1 / rtotal_1;
-
   Ia = itotal_1 * (r2 / (r2 + r3));
   IaM = (Ia * 1000).toFixed(4);
-  console.log('-----------------------------');
-  console.log('R1: ' + r1);
-  console.log('R2: ' + r2);
-  console.log('R3: ' + r3);
-  console.log('-----------------------------');
-  console.log('R Total: ' + rtotal_1);
-  console.log('I Total: ' + itotal_1);
-  console.log('IA: ' + Ia);
-  console.log('Ia(mA): ' + IaM);
-  console.log('-----------------------------');
-
-
   rtotal_2 = ((r1 * r3) / (r1 + r3)) + r2;
-
   itotal_2 = e2 / rtotal_2;
-
   Ib = itotal_2 * (r1 / (r1 + r3));
   IbM = (Ib * 1000).toFixed(4);
-
   I3 = Ia + Ib;
   I3M = (I3 * 1000).toFixed(4);
-
-  console.log('-----------------------------');
-  console.log('R Total: ' + rtotal_2);
-  console.log('I Total: ' + itotal_2);
-  console.log('Ib: ' + Ib);
-  console.log('Ib(mA): ' + IbM);
-  console.log('-----------------------------');
-  console.log('I3: ' + I3);
-  console.log('I3(mA): ' + I3M);
-
 
   res.render("pages/lab1_result", {
     IaM: IaM,
@@ -110,9 +84,7 @@ router.post('/cal_lab2', urlencodedParser, function (req, res, next) {
   var r1 = parseFloat(req.body.r1);
   var r2 = parseFloat(req.body.r2);
   var r3 = parseFloat(req.body.r3);
-
   var rl = parseFloat(req.body.rl);
-
   var e1 = parseFloat(req.body.e1);
   var e2 = parseFloat(req.body.e2);
 
@@ -175,12 +147,8 @@ router.post('/cal_lab4', urlencodedParser, function (req, res, next) {
   var VlD = 0;
 
   Rtotal = rth1 + rll;
-  Console.log("R Total:" + Rtotal);
   Ith = eth / Rtotal;
-  Console.log("ITH:" + Ith);
   vl = Ith * rll;
-  Console.log("VL:" + Rtovltal);
-
   VlD = (vl).toFixed(2);
 
 
